66c39476e6b0c6e069219e8d675256c05b2ef841
Vinny <92878154+VinnyBonner@users.noreply.github.com>
2022-03-31 11:53:25 -0700

    Update __init__.py

--- SendEmail/__init__.py
+++ SendEmail/__init__.py
@@ -53,8 +53,22 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     
     SendEmail()
 
-    return func.HttpResponse("This HTTP triggered function executed successfully.", status_code=200)
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
 
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully.",
+             status_code=200
+        )
 
 def SendEmail(): 
     
